e05fbc3751e22c77806108870ac68b40dceb6a04
Clifford Manu <cliffordmanu7@gmail.com>
2025-02-10 16:40:54 +0000

    Create Listing step2

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,9 @@
 import "../styles/CreateListing.scss";
 import Navbar from "../components/Navbar";
-import { categories, types } from "../data";
+import { categories, facilities, types } from "../data";
+
+import { RemoveCircleOutline, AddCircleOutline } from "@mui/icons-material";
+import variables from "../styles/variables.scss";
 
 const CreateListing = () => {
   return (
@@ -26,11 +29,12 @@ const CreateListing = () => {
             <h3>What type of place will guest have?</h3>
             <div className="type-list">
               {types?.map((item, index) => (
-                <div className="type">
+                <div className="type" key={index}>
                   <div className="type_text">
                     <h4>{item.name}</h4>
                     <p>{item.description}</p>
                   </div>
+                  <div className="type_icon">{item.icon}</div>
                 </div>
               ))}
             </div>
@@ -52,7 +56,7 @@ const CreateListing = () => {
                 <p>Apartment , Suite, etc. (if applicable)</p>
                 <input
                   type="text"
-                  placeholder="City"
+                  placeholder="Apartment , Suite, etc. (if applicable)"
                   name="aptSuite"
                   required
                 />
@@ -85,7 +89,108 @@ const CreateListing = () => {
                 />
               </div>
             </div>
+
+            <h3>Share some basics about your place</h3>
+            <div className="basics">
+              <div className="basic">
+                <p>Guests</p>
+                <div className="basic_count">
+                  <RemoveCircleOutline
+                    sx={{
+                      fontSize: "25",
+                      cursor: "pointer",
+                      "&:hover": { color: variables.pinkred },
+                    }}
+                  />
+                  <p>1</p>
+                  <AddCircleOutline
+                    sx={{
+                      fontSize: "25",
+                      cursor: "pointer",
+                      "&:hover": { color: variables.pinkred },
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="basic">
+                <p>Bedrooms</p>
+                <div className="basic_count">
+                  <RemoveCircleOutline
+                    sx={{
+                      fontSize: "25",
+                      cursor: "pointer",
+                      "&:hover": { color: variables.pinkred },
+                    }}
+                  />
+                  <p>1</p>
+                  <AddCircleOutline
+                    sx={{
+                      fontSize: "25",
+                      cursor: "pointer",
+                      "&:hover": { color: variables.pinkred },
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="basic">
+                <p>Beds</p>
+                <div className="basic_count">
+                  <RemoveCircleOutline
+                    sx={{
+                      fontSize: "25",
+                      cursor: "pointer",
+                      "&:hover": { color: variables.pinkred },
+                    }}
+                  />
+                  <p>1</p>
+                  <AddCircleOutline
+                    sx={{
+                      fontSize: "25",
+                      cursor: "pointer",
+                      "&:hover": { color: variables.pinkred },
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="basic">
+                <p>Bathrooms</p>
+                <div className="basic_count">
+                  <RemoveCircleOutline
+                    sx={{
+                      fontSize: "25",
+                      cursor: "pointer",
+                      "&:hover": { color: variables.pinkred },
+                    }}
+                  />
+                  <p>1</p>
+                  <AddCircleOutline
+                    sx={{
+                      fontSize: "25",
+                      cursor: "pointer",
+                      "&:hover": { color: variables.pinkred },
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
+
+            <div className="create-listing_step2">
+                <h2>Step 2: Make your place stand Out</h2>
+                <hr />
+                <h3>Tell guest what your place has to offer</h3>
+                <div className="amenities">
+                    {facilities?.map((item, index) => (
+                        <div className="facility" key={index}>
+                            <div className="facility_icon">{item.icon}</div>
+                            <p>{item.name}</p>
+                        </div>
+                        ))}
+                </div>
+            </div>
         </form>
       </div>
     </>

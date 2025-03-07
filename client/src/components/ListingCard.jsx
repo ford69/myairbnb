d8cf7942e77fbcc8ea02b40e3055f87aa8738fb7
Clifford Manu <cliffordmanu7@gmail.com>
2025-03-07 14:28:04 +0000

    Card listing resolved

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import {useState} from "react";
 import "../styles/ListingCard.scss";
 import { IoArrowBack, IoArrowForward } from "react-icons/io5";
 
-const ListingCard = () => ({
+const ListingCard = ({
   listingId,
     creator,
     listingPhotoPaths,
@@ -13,17 +13,29 @@ const ListingCard = () => ({
     type,
     price,
  }) => {
+  // SlIDER FOR IMAGES
+  const[currentIndex,setCurrentIndex] = useState(0);
+
+  const goToPrevSlide = () => {
+      setCurrentIndex(
+        (prevIndex) =>
+          (prevIndex - 1 + listingPhotoPaths.length) % listingPhotoPaths.length)
+  } 
+
+  const goToNextSlide = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % listingPhotoPaths.length)
+  }
   return (
     <div className="listing-card">
       <div className="slider-container">
         <div className="slider">
         {listingPhotoPaths?.map((photo, index) => (
             <div key={index} className="slide">
-                <img src={`http://localhost:3001/${photo.replace("public", "")}`} alt={`photo ${index + 1}`} />
+                <img src={`http://localhost:3001/${photo.replace("public", "")}`} alt={`photos ${index + 1}`} />
                 <div className="prev-button">
                     <IoArrowBack sx={{ fontSize: "15px"}} />
                 </div>
-                <div className="prev-button">
+                <div className="next-button">
                     <IoArrowForward sx={{ fontSize: "15px"}} />
                 </div>
             </div>
